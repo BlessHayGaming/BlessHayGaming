ffbd2ab9a872c36ee8f1083a806ab13a1752432f
Simon Milfred <simonmilfred@gmail.com>
2019-12-15 18:10:47 +0100

    Updated package

--- gridsome.config.js
+++ gridsome.config.js
@@ -19,7 +19,7 @@ function addStyleResource (rule) {
 module.exports = {
     siteName: 'Bless Hay Gaming',
     siteDescription: 'The home of Bless Hay Gaming.',
-    siteUrl: 'https://www.blesshaygaming.com',
+    siteUrl: 'https://blesshaygaming.github.io/',
     plugins: [
         {
             use: '@gridsome/source-filesystem',
